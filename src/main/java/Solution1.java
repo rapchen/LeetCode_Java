3aefb4535ad4e7ce75ce764f26e431ea9f97275b
crw <18800128128@163.com>
2018-03-04 18:20:34 +0800

    problem 1

---
+++ src/main/java/Solution1.java
@@ -0,0 +1,29 @@
+import java.util.*;
+
+/**
+ * @author Chen Runwen
+ * @version 1.0 2018/3/4
+ */
+public class Solution1 {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            Integer num = nums[i];
+            if (map.containsKey(target - num)) {
+                result[0] = map.get(target - num);
+                result[1] = i;
+                return result;
+            }
+            map.put(num, i);
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 4};
+        int target = 6;
+        Solution1 solution = new Solution1();
+        System.out.println(Arrays.toString(solution.twoSum(nums, target)));
+    }
+}
